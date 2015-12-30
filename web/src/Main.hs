766244e960f4802615211a204b65ec10dcffa996
Kynan Rilee <kynan.rilee@gmail.com>
2015-12-30 13:05:17 -0800

    xhr on page load, parse and display results

--- web/src/Main.hs
+++ web/src/Main.hs
@@ -6,14 +6,36 @@ import Reflex
 import Reflex.Dom
 import Reflex.Dom.Xhr
 import Data.Text
+import Text.Parsec
+import Data.Either.Combinators (rightToMaybe)
+import Control.Monad
+import CsgoGuideParser
 
 main :: IO ()
 main = mainWidget appWidget
 
 appWidget :: (MonadWidget t m) => m ()
 appWidget = el "h1" $ do
-  buttonEvent <- button "click here"
-  let req = xhrRequest "GET" "public/blah.txt" def
-  res <- performRequestAsync (tag (constant req) buttonEvent)
-  contents <- holdDyn "hello" $ fmap (show . _xhrResponse_body) res
-  dynText contents
+  bld <- getPostBuild
+  nades_ <- getNadeInfos (tag (constant "public/cache.txt") bld)
+  nades <- holdDyn [] nades_
+  nadeWidgets <- mapDyn (mapM_ nadeInfoWidget) nades
+  void . dyn $ nadeWidgets
+
+getResource :: (MonadWidget t m) => Event t String -> m (Event t Text)
+getResource address = do
+  res <- performRequestAsync $ fmap makeReq address
+  return $ fmapMaybe _xhrResponse_body res
+  where makeReq addr = xhrRequest "GET" addr def
+
+getNadeInfos :: (MonadWidget t m) => Event t String -> m (Event t [NadeInfo])
+getNadeInfos address = do
+  contents <- getResource address
+  let nades = fmap (runParser nadeInfos () "" . unpack) contents
+  return $ fmapMaybe rightToMaybe nades
+
+nadeInfoWidget :: (MonadWidget t m) => NadeInfo -> m ()
+nadeInfoWidget (NadeInfo img desc tags) = do
+  el "div" $ do
+    elAttr "img" ("src" =: img) (return ())
+    el "div" $ text desc
