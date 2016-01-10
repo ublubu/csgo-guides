576eaf0191dbcab3899c7f2d4c51926b6ae4df7d
Kynan Rilee <kynan.rilee@gmail.com>
2016-01-09 16:21:49 -0800

    parse Google's response and extract user ID

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeFamilies  #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Main where
 
@@ -20,6 +19,8 @@ import Data.List
 import Data.Maybe
 import Data.Monoid
 import Data.Text (Text, pack, unpack)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import Data.Text.Lazy (fromStrict)
 import Data.Text.Lazy.Encoding (encodeUtf8, decodeUtf8)
 import qualified Data.ByteString.Char8 as BS
@@ -27,12 +28,14 @@ import Data.ByteString.Lazy (ByteString)
 import GHC.Generics
 import Network.HTTP.Client.Conduit (Manager, withManager, HasHttpManager(..), newManager, withResponse, Response)
 import qualified Network.HTTP.Client.Conduit as H
+import qualified Network.HTTP.Types.Status as H
 import Network.Wai
 import Network.Wai.Handler.Warp (run)
 import Servant
 import Servant.Server
 
-data AppConfig = AppConfig { _appConfigHttpManager :: Manager }
+data AppConfig = AppConfig { _appConfigHttpManager :: Manager
+                           , _appConfigGoogleClientId :: Text }
 data AppError = Invalid Text | WrappedServantErr ServantErr
 type App = ReaderT AppConfig (ExceptT AppError IO)
 
@@ -55,14 +58,34 @@ readerServer =
           Nothing -> throwError $ Invalid "missing token in queryparams"
           Just t -> askGoogle t
 
+data GoogleTokenInfo = GoogleTokenInfo { _googleTokenAud :: Text
+                                       , _googleTokenSub :: Text
+                                       } deriving (Show, Eq)
+
+instance FromJSON GoogleTokenInfo where
+  parseJSON (Object v) = GoogleTokenInfo <$> v .: "aud" <*> v .: "sub"
+  parseJSON _ = mzero
+
 askGoogle :: String -> App String
 askGoogle token = do
   request <- liftIO $ H.parseUrl "https://www.googleapis.com/oauth2/v3/tokeninfo"
   let r = H.setQueryString [("id_token", Just $ BS.pack token)] request
   withResponse r processGoogleResponse
 
-processGoogleResponse :: H.Response (ConduitM i BS.ByteString App ()) -> App String
-processGoogleResponse = return . show . H.responseStatus
+processGoogleResponse :: H.Response (ConduitM () BS.ByteString App ()) -> App String
+processGoogleResponse res = do
+  let bodyProducer = H.responseBody res
+  body <- runConduit $ bodyProducer =$= C.fold mappend mempty
+  when (H.responseStatus res /= H.status200) $ throwError
+    $ Invalid "google says your token is no good"
+  let tokenInfo = (decodeStrict' body :: Maybe GoogleTokenInfo)
+  case tokenInfo of
+    Nothing -> throwError $ Invalid "google's response wasn't token info"
+    Just t -> do
+      config <- ask
+      when (_appConfigGoogleClientId config /= _googleTokenAud t)
+        $ throwError $ Invalid "your token isn't for this app"
+      return . show $ t
 
 runAppT :: App a -> EitherT ServantErr IO a
 runAppT action = do
@@ -76,11 +99,15 @@ runAppT action = do
 textToBSL :: Text -> ByteString
 textToBSL = encodeUtf8 . fromStrict
 
+-- TODO: ssl for the Manager
 defaultAppConfig :: (MonadIO m) => m AppConfig
-defaultAppConfig = fmap AppConfig newManager
+defaultAppConfig = do
+  clientId <- liftIO . fmap T.strip $ T.readFile "../clientid.txt"
+  manager <- newManager
+  return $ AppConfig manager clientId
 
 server' :: Server API
-server' = enter (Nat runAppT) readerServer :<|> serveDirectory "static"
+server' = enter (Nat runAppT) readerServer :<|> serveDirectory "../static"
 
 app :: Application
 app = serve api $ server'
