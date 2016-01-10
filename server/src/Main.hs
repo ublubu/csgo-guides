ecda1bf70a5c79cf2ede1ddaa556999f0f88a0f6
Kynan Rilee <kynan.rilee@gmail.com>
2016-01-09 17:07:27 -0800

    set browser cookie on verifying Google login

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -24,6 +24,7 @@ import qualified Data.Text.IO as T
 import Data.Text.Lazy (fromStrict)
 import Data.Text.Lazy.Encoding (encodeUtf8, decodeUtf8)
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy as BSL
 import Data.ByteString.Lazy (ByteString)
 import GHC.Generics
 import Network.HTTP.Client.Conduit (Manager, withManager, HasHttpManager(..), newManager, withResponse, Response)
@@ -33,16 +34,19 @@ import Network.Wai
 import Network.Wai.Handler.Warp (run)
 import Servant
 import Servant.Server
+import Web.ClientSession
 
 data AppConfig = AppConfig { _appConfigHttpManager :: Manager
-                           , _appConfigGoogleClientId :: Text }
+                           , _appConfigGoogleClientId :: Text
+                           , _appConfigClientSessionKey :: Key }
 data AppError = Invalid Text | WrappedServantErr ServantErr
 type App = ReaderT AppConfig (ExceptT AppError IO)
 
 instance HasHttpManager AppConfig where
   getHttpManager = _appConfigHttpManager
 
-type ReaderAPI = "tokensignin" :> QueryParam "idtoken" String :> Get '[JSON] String
+type SetCookied a = Headers '[Header "Set-Cookie" String] a
+type ReaderAPI = "tokensignin" :> QueryParam "idtoken" String :> Get '[JSON] (SetCookied String)
 type StaticAPI = Raw
 type API = ReaderAPI :<|> StaticAPI
 
@@ -53,7 +57,7 @@ readerServer :: ServerT ReaderAPI App
 readerServer =
   tokensignin
 
-  where tokensignin :: Maybe String -> App String
+  where tokensignin :: Maybe String -> App (SetCookied String)
         tokensignin token = case token of
           Nothing -> throwError $ Invalid "missing token in queryparams"
           Just t -> askGoogle t
@@ -66,13 +70,33 @@ instance FromJSON GoogleTokenInfo where
   parseJSON (Object v) = GoogleTokenInfo <$> v .: "aud" <*> v .: "sub"
   parseJSON _ = mzero
 
-askGoogle :: String -> App String
+data CookieData = CookieData { _cookieDataUserId :: Text }
+
+instance FromJSON CookieData where
+  parseJSON (Object v) = CookieData <$> v .: "userid"
+  parseJSON _ = mzero
+
+instance ToJSON CookieData where
+  toJSON (CookieData userid) = object ["userid" .= userid]
+
+getCookieData :: GoogleTokenInfo -> App CookieData
+getCookieData = return . CookieData . _googleTokenSub
+
+encryptCookie :: CookieData -> App String
+encryptCookie cookie = do
+  iv <- liftIO randomIV
+  config <- ask
+  return . BS.unpack $ encrypt (_appConfigClientSessionKey config) iv (BSL.toStrict $ encode cookie)
+
+askGoogle :: String -> App (SetCookied String)
 askGoogle token = do
   request <- liftIO $ H.parseUrl "https://www.googleapis.com/oauth2/v3/tokeninfo"
   let r = H.setQueryString [("id_token", Just $ BS.pack token)] request
-  withResponse r processGoogleResponse
+  tokenInfo <- withResponse r processGoogleResponse
+  cookie <- encryptCookie =<< getCookieData tokenInfo
+  return $ addHeader cookie (unpack . _googleTokenSub $ tokenInfo)
 
-processGoogleResponse :: H.Response (ConduitM () BS.ByteString App ()) -> App String
+processGoogleResponse :: H.Response (ConduitM () BS.ByteString App ()) -> App GoogleTokenInfo
 processGoogleResponse res = do
   let bodyProducer = H.responseBody res
   body <- runConduit $ bodyProducer =$= C.fold mappend mempty
@@ -85,7 +109,7 @@ processGoogleResponse res = do
       config <- ask
       when (_appConfigGoogleClientId config /= _googleTokenAud t)
         $ throwError $ Invalid "your token isn't for this app"
-      return . show $ t
+      return t
 
 runAppT :: App a -> EitherT ServantErr IO a
 runAppT action = do
@@ -104,7 +128,8 @@ defaultAppConfig :: (MonadIO m) => m AppConfig
 defaultAppConfig = do
   clientId <- liftIO . fmap T.strip $ T.readFile "../clientid.txt"
   manager <- newManager
-  return $ AppConfig manager clientId
+  key <- liftIO $ getKey "../sessionkey.txt"
+  return $ AppConfig manager clientId key
 
 server' :: Server API
 server' = enter (Nat runAppT) readerServer :<|> serveDirectory "../static"
