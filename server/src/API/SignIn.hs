640b5172e07ec2a183b06e731bdc2546328ccfd4
Kynan Rilee <kynan.rilee@gmail.com>
2016-01-10 19:01:24 -0800

    use Generic deriving to get FromJSON and ToJSON

--- server/src/API/SignIn.hs
+++ server/src/API/SignIn.hs
@@ -9,16 +9,13 @@ module API.SignIn where
 import Control.Monad
 import Data.Aeson
 import Data.Text (Text)
+import GHC.Generics
 import Servant.API
 
-data CookieData = CookieData { _cookieDataUserId :: Text } deriving (Show, Eq)
+data CookieData = CookieData { _cookieDataUserId :: Text } deriving (Show, Eq, Generic)
 
-instance FromJSON CookieData where
-  parseJSON (Object v) = CookieData <$> v .: "userid"
-  parseJSON _ = mzero
-
-instance ToJSON CookieData where
-  toJSON (CookieData userid) = object ["userid" .= userid]
+instance FromJSON CookieData
+instance ToJSON CookieData
 
 type SetCookied a = Headers '[Header "Set-Cookie" Text] a
 type SignInAPI = "tokensignin" :> QueryParam "idtoken" Text :> Get '[JSON] (SetCookied Text)
